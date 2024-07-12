f439b198ae178d9c772540d77a731c5bcd8dfa7b
José Vitor de Freitas Cunha <30449427+josevitoor@users.noreply.github.com>
2024-07-12 19:03:42 -0300

    Add files via upload

---
+++ IProdutoRepository.cs
@@ -0,0 +1,10 @@
+﻿using APICatalogo.Models;
+
+namespace APICatalogo.Repositories;
+
+public interface IProdutoRepository : IRepository<Produto>
+{
+    IEnumerable<Produto> GetProdutosPorCategoria(int id);
+    PagedList<Produto> GetProdutos(ProdutosParameters produtosParams);
+    PagedList<Produto> GetProdutosFiltroPreco(ProdutosFiltroPreco produtosFiltroParams);
+}
